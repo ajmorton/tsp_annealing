bc86befd776c52dae158a5d8aa79a832a3c9a128
ashishk07 <ashishk07@live.com>
2016-10-21 18:22:09 +1100

    Add files via upload

--- code/base.cpp
+++ code/base.cpp
@@ -2,6 +2,13 @@
 // - times are wrong, too slow
 // - temperature printing alignment
 
+
+// TODO -- Ashish 
+// Need to see if initial path needs to be the order in which the nodes were entered OR use the k-nearest neighbour method ( needs extra implementation and more runtime too maybe? )
+// 
+
+
+
 // tsp.cpp
 // Traveling salesman problem with simulated annealing
 
@@ -24,10 +31,14 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <sys/time.h>
 #include <utility>
+#include <algorithm>
+#include "omp.h"
+//#include "mpi.h"
 
 typedef std::pair<int, int> int_pair;
+using namespace std;
 
 // annealing temperature constants
 #define TEMP_START    100
@@ -36,6 +47,7 @@ typedef std::pair<int, int> int_pair;
 
 // set to true to use hill climbing instead of annealing
 #define HILL_CLIMBING false
+#define UNIFORM_V 0.1
 
 // what to print to stdout
 // 0: only print the min path cost found
@@ -45,6 +57,7 @@ typedef std::pair<int, int> int_pair;
 // NOTE: io is expensive, VERBOSITY 3 considerably slows run time
 #define VERBOSITY 3
 
+struct timeval start, end; // track start and end time of program run
 
 class TSP {
     private:
@@ -64,6 +77,10 @@ class TSP {
         TSP();                      // constructor
        ~TSP();                      // destructor
 
+        double metropolis(float,float,double);          // Metropolis Algorithm -- The Basis of both SA & PT
+        //void pa_openMP();
+        //void pa_MPI();                  // Find the min path via Parallel Tempering -- According to the method specified in the paper
+        void simul_anneal(int,int,int);        // Find the min path via Simulated Annealing -- According to the method specified in the paper
         void anneal();              // find the min path via annealing
         void read_in();             // read in the problem from file
 
@@ -71,6 +88,10 @@ class TSP {
 
 };
 
+double timedifference_msec(struct timeval start, struct timeval end) {
+                // seconds             +  // milliseconds
+    return (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000000.0;
+}
 
 // get the path cost of a tsp path
 double TSP::cost(int path[]){
@@ -141,7 +162,9 @@ void TSP::anneal(){
             min_cost = new_cost;
 
             if(VERBOSITY >= 2){
-                printf("\rtime: %.3fs\tpath: %.3f\n", (double)(clock() - start_time)/CLOCKS_PER_SEC, min_cost);
+                gettimeofday(&end, 0);
+                double elapsed = timedifference_msec(start, end);
+                printf("\rtime: %.3fs\tpath: %.3f\n", elapsed, min_cost);
             }
         }
 
@@ -173,7 +196,10 @@ void TSP::anneal(){
         // for(int i = 0; i < n+1; ++i){
         //     printf("%d ", min_path[i]);
         // }
-        printf("\ntime: %.3fs\n", (double)(clock() - start_time)/CLOCKS_PER_SEC);
+
+        gettimeofday(&end, 0);
+        double elapsed = timedifference_msec(start, end);
+        printf("\ntime: %.3fs\n", elapsed);
     } else {
         printf("%.3f\n", min_cost);
     }
@@ -193,7 +219,7 @@ TSP::TSP(){
     // set static seed
     srand(1001);
 
-    start_time = clock();
+    gettimeofday(&start, 0);
 }
 
 // destructor
@@ -243,9 +269,138 @@ double TSP::distance(int_pair a, int_pair b){
     return sqrt(x_dist * x_dist + y_dist * y_dist);
 }
 
+
+double TSP::metropolis(float new_cost, float old_cost, double temp) {
+     // Acceptance probability from old path to new path
+    return exp((old_cost - new_cost)/temperature);   // change to min(1,exp((old_cost - new_cost)/temperature));
+}
+
+void TSP::simul_anneal(int total_steps, int step_length, int cooling_rate){
+
+    // instantiate paths
+    cur_path = new int[n+1];
+    new_path = new int[n+1];
+    //min_path = new int[n+1];
+
+    // always use city 0 as the start and end point
+    // this does not change throughout the program
+    cur_path[0] = cur_path[n] = 0;
+    new_path[0] = new_path[n] = 0;
+    //min_path[0] = min_path[n] = 0;
+
+    // use the order of input as the start implementation
+    for(int i = 1; i < n; ++i){
+        cur_path[i] = i;
+        new_path[i] = i;
+       // min_path[i] = i;
+    }
+
+    float init_cost = cost(cur_path);
+    int step_temp; 
+    float cur_cost = init_cost;
+    float min_cost = init_cost;
+    float new_cost = init_cost;
+
+    double init_temp = 1.5/sqrt(n);  // DEFINE initial temperature according to number of cities
+    double curr_temp; 
+
+    step_temp = total_steps/step_length - 1; 
+
+    for (int i=0; i<step_temp ;i++){
+        curr_temp = init_temp * pow (cooling_rate, i); 
+
+        for (int j=0; j<=step_length; j++){
+
+            int city1 = (rand() % (n-1)) + 1;
+            int city2 = (rand() % (n-1)) + 1;
+            swap(city1, city2, cur_path);
+
+            new_cost = cost(cur_path);
+            
+            if(new_cost < min_cost){
+                for(int i=1; i < n; ++i){
+                    min_path[i] = cur_path[i];
+                }
+                min_cost = new_cost;
+            }
+
+            if (metropolis(new_cost,cur_cost,curr_temp) > UNIFORM_V){
+                //cur_path = new_path;
+                cur_cost = new_cost; 
+            }
+            else swap(city1,city2,cur_path);
+        }
+
+    }
+
+    // Didn't go into implementing the VERBOSITY prints needed, stuck to  -- VERBOSITY = 1
+
+    printf("\n\nshortest path: %.3f\n", min_cost);
+    printf("initial  path: %.3f\n", init_cost);
+
+
+}
+
+
+void TSP::pa_openMP(int total_steps, int swap_length, int cooling_rate){
+
+    cur_path = new int[n+1];
+    new_path = new int[n+1];
+    //min_path = new int[n+1];
+
+    // always use city 0 as the start and end point
+    // this does not change throughout the program
+    cur_path[0] = cur_path[n] = 0;
+    new_path[0] = new_path[n] = 0;
+        //min_path[0] = min_path[n] = 0;
+
+    // use the order of input as the start implementation
+    for(int i = 1; i < n; ++i){
+        cur_path[i] = i;
+        new_path[i] = i;
+        //min_path[i] = i;
+    }
+
+    float init_cost = cost(cur_path);
+    int step_temp; 
+    float cur_cost = init_cost;
+    float min_cost = init_cost;
+    float new_cost = init_cost;
+
+    double init_temp = 1.5/sqrt(n);  // DEFINE initial temperature according to number of cities
+    double curr_temp; 
+
+    step_temp = total_steps/swap_length - 1; 
+    int temp_array[5] = {0.0025, 0.004, 0.006, 0.008,0.01}; 
+
+    for (int i=0; i<step_temp ;i++){
+        //vector<vector<int>> distance_array(n, vector<int>(n+1));
+        curr_temp = init_temp * pow (cooling_rate, i); 
+        //#pragma omp parallel for firstprivate (curr_temp,cur_path,new_path) lastprivate (cur_path) (5)
+        for (j=0; j<=swap_length; j++){
+
+            int city1 = (rand() % (n-1)) + 1;
+            int city2 = (rand() % (n-1)) + 1;
+            swap(city1, city2, cur_path);
+
+            new_cost = cost(new_path);
+
+            if (metropolis(new_cost,cur_cost,curr_temp) > UNIFORM_V){
+            //    cur_path = new_path;
+                cur_cost = new_cost; 
+            }
+            else swap(city1,city2,cur_path);
+            // HERE,  AFTER the j-loop finishes execution, we need to exchange the path of any two RANDOM paths
+        }
+            // TO DO TO DO 
+    }
+
+}*/
+
 int main(){
     TSP tsp;
     tsp.read_in();
     tsp.anneal();
+    tsp.simul_anneal(1000000,160000,0.95);
     return 0;
 }
